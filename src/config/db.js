e143c25617a589d227672457d3d20eefd2da4736
Carlos Barçante <barcantecarlosdev@gmail.com>
2023-05-16 13:07:25 -0300

    feat: add app setup

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const { MONG0_URL } = process.env;
+const { MONGO_URL } = process.env;
 
 const connectToDB = async () => {
     try {
-        await mongoose.connect(MONG0_URL);
+        await mongoose.connect(MONGO_URL);
         console.log('Database connected...');
     } catch (error) {
         console.log(error);
